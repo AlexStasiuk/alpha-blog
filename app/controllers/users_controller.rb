0928ab499f6f67933c8c4d83625e248a817c1fb1
AlexStasiuk <stasiuksasha8@gmail.com>
2021-04-11 21:51:27 +0300

    add listing of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -29,6 +30,11 @@ class UsersController < ApplicationController
     else
         render 'edit'
     end
+  end
+
+  def index
+    @users = User.all
+    
   end
   private
   def user_params
