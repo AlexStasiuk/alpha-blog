85ce66c3e924c6c92fe99da4cf075fe8bfe20d9d
AlexStasiuk <stasiuksasha8@gmail.com>
2021-04-14 01:39:39 +0300

    add update/edit page and actions which handle that request

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
+  before_action :set_category, only: [:edit, :update, :show]
   def new
       @category = Category.new
   end
@@ -19,12 +20,27 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
+    
     @articles = @category.articles.paginate(page: params[:page], per_page: 2)
   end
   
-  private
+  def edit
+    
+  end
 
+  def update
+    if @category.update(category_params)
+      flash[:notice] = "Successfully updated category"
+    else
+      flash[:alarm] = "Something went wrong while updating category"
+    end
+    redirect_to @category
+  end
+  private
+  def set_category
+    @category = Category.find(params[:id])
+  end
+  
   def category_params
     params.require(:category).permit(:name)
   end
